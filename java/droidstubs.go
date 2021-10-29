f8e2532a77f1585086822beafb8512ddbdd93f42
LeddaZ <leonardoledda@gmail.com>
2021-10-29 16:54:04 +0200

    soong: increase heap size for metalava
    * Fixes building on systems with < 16GB RAM
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- java/droidstubs.go
+++ java/droidstubs.go
@@ -517,8 +517,10 @@ func metalavaCmd(ctx android.ModuleContext, rule *android.RuleBuilder, javaVersi
 
 	cmd.BuiltTool("metalava").ImplicitTool(ctx.Config().HostJavaToolPath(ctx, "metalava.jar")).
 		Flag(config.JavacVmFlags).
-		Flag(config.MetalavaAddOpens).
-		FlagWithArg("--java-source ", javaVersion.String()).
+		Flag("-J-Xmx6114m").
+		Flag("-J--add-opens=java.base/java.util=ALL-UNNAMED").
+		FlagWithArg("-encoding ", "UTF-8").
+		FlagWithArg("-source ", javaVersion.String()).
 		FlagWithRspFileInputList("@", android.PathForModuleOut(ctx, "metalava.rsp"), srcs).
 		FlagWithInput("@", srcJarList)
 
